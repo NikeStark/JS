ee15ee64bf5d452ed224560d5e995023d107bff8
Aliaksandr Kazutsin <kazutin.1993@mail.ru>
2018-09-26 16:02:39 +0300

    initial_files_for_learn

--- main.js
+++ main.js
@@ -1,2 +1,36 @@
-Hello World!!!
-You did it!!!!!!!!!!!!!!
+//Переменные
+var one = 'You ';
+var two = 'needta ';
+var three = 'act ';
+var four = 'Look on this';
+alert(four);
+console.log(one+two+three);
+
+/*var bool = true;
+console.log(bool);*/
+
+/*var name = prompt ("How do you name?");
+alert("My name is "+ name);
+var age = prompt ("How old are you?");
+alert(age+" years old");*/
+
+var firstNum = 12;
+var secondNum = 24;
+firstNum -= 12;
+secondNum++;
+document.write(Math.PI+"<br>");
+document.write(Math.round (2.49)+"<br>");
+document.write("My result: ", +firstNum+secondNum+"<br>");
+
+//Массивы
+var colors = ["Red","Blue","Green","Pink"];
+colors[4] =  "Yellow";
+document.write(colors[4]);
+
+var Colors = new Array ("Red","Blue","Green");
+Colors[3] = "Orange";
+Colors.push("White");
+console.log(Colors[4]);
+
+//Циклы
+
