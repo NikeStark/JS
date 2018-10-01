2ce6ee72aa28554396ed85452c2afd0ab77511bf
Aliaksandr Kazutsin <kazutin.1993@mail.ru>
2018-10-01 16:24:10 +0300

    continue_js

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ document.write("My result: ", +firstNum+secondNum+"<br>");
 //Массивы
 var colors = ["Red","Blue","Green","Pink"];
 colors[4] =  "Yellow";
-document.write(colors[4]);
+document.write(colors[4]+"<br>");
 
 var Colors = new Array ("Red","Blue","Green");
 Colors[3] = "Orange";
@@ -34,6 +34,62 @@ console.log(Colors[4]);
 
 //Циклы
 
-for (var i = 100; i > 10; i+=10){
+var i = 100
+do{
 console.log(i);
+}while(i<99);
+
+/*var i = 1;
+while(i<=100){
+console.log(i);
+i+=2;
+}*/
+
+/*for (var i = 100; i >= 1; i-=10){
+console.log(i);
+}*/
+
+var different_colors = new Array ("Red","Green","Blue");
+	for (var i = 0; i < different_colors.length;i++)
+		console.log(different_colors[i]+" color");
+	
+//Операторы условий
+if(1 != 1){
+	document.write("It's truth"+"<br>");
+}else if (1==1){
+	document.write("It's good"+"<br>");
+} else {
+	document.write("It's false"+"<br>");
 }
+
+var a = 10;
+var b = 20;
+if(a>b){
+document.write(a + ">" + b + "<br>");
+}else if(a == b){
+	document.write(a + "==" + b + "<br>");
+}else{
+	document.write(a + "<" + b + "<br>");
+}
+
+var bool = false;
+if((!bool || a != 10) && b == 20){
+	document.write (bool + " is false"+ "<br>")
+}
+
+var res = 5;
+switch(res){
+	case 1:
+	alert("Res from ome case");
+	break;
+	case 2:
+	alert("Res from two case");
+	break;
+	case 3:
+	alert("Res from three case");
+	break;
+	default:
+	alert("Res is unknown")
+}
+
+// Функции
